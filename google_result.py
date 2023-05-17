cd38593645e59c246ffe64288cd283587afe9c9e
JosephMakkimane <30369487+JosephMakkimane@users.noreply.github.com>
2023-05-17 16:08:38 +0530

    Update google_result.py

--- google_result.py
+++ google_result.py
@@ -4,7 +4,7 @@ except ImportError:
 	print("No module named 'google' found")
 
 # to search
-query = "Geeksforgeeks"
+query = ""
 
 for j in search(query, tld="co.in", num=1, stop=10, pause=2):
 	print(j)
